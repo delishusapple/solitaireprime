7ea2da5927436865eef63a40d1c50a90c7aa3a15
delishusapple <alanphuvan@gmail.com>
2021-03-30 15:02:13 -0700

    removed unnecessary comments

--- solitaireprime.cpp
+++ solitaireprime.cpp
@@ -1,10 +1,3 @@
-//Alan Van
-//Class (CECS 282-01)
-//Project Name (Prog 1 – Solitaire Prime)
-//Due Date 2/8/21
-//
-//I certify that this program is my own original work. I did not copy any part of this program from            
-//any other source. I further certify that I typed each and every line of code in this program
 #include <iostream>
 #include <string>
 #include "Card.h"
